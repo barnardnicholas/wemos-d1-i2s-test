eed2e15146e3ad69626f446f8774e41c2907b6b0
Nick Barnard <55743033+barnardnicholas@users.noreply.github.com>
2023-11-29 16:59:23 -0500

    TODO - Line 285 resolves to NaN

--- wemos-d1-i2s-test/wemos-d1-i2s-test.ino
+++ wemos-d1-i2s-test/wemos-d1-i2s-test.ino
@@ -8,27 +8,29 @@
  *
   ******************************************************************************/
 // #include <ESP8266WiFi.h>
+#include "sos-iir-filter.h"
+#include "math.h"
 
 extern "C" {
 #include "user_interface.h"
 #include "i2s_reg.h"
 #include "slc_register.h"
 #include "esp8266_peri.h"
-void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
+  void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
 }
 
 // #define DEBUG
 
-#define I2S_CLK_FREQ      160000000  // Hz
-#define I2S_24BIT         3     // I2S 24 bit half data
-#define I2S_LEFT          2     // I2S RX Left channel
+#define I2S_CLK_FREQ 160000000  // Hz
+#define I2S_24BIT 3             // I2S 24 bit half data
+#define I2S_LEFT 2              // I2S RX Left channel
 
-#define I2SI_DATA         12    // I2S data on GPIO12
-#define I2SI_BCK          13    // I2S clk on GPIO13
-#define I2SI_WS           14    // I2S select on GPIO14
+#define I2SI_DATA 12  // I2S data on GPIO12
+#define I2SI_BCK 13   // I2S clk on GPIO13
+#define I2SI_WS 14    // I2S select on GPIO14
 
-#define SLC_BUF_CNT       8     // Number of buffers in the I2S circular buffer
-#define SLC_BUF_LEN       64    // Length of one buffer, in 32-bit words.
+#define SLC_BUF_CNT 8   // Number of buffers in the I2S circular buffer
+#define SLC_BUF_LEN 64  // Length of one buffer, in 32-bit words.
 
 /**
  * Convert I2S data.
@@ -41,16 +43,26 @@ void rom_i2c_writeReg_Mask(int, int, int, int, int, int);
 #define convert(sample) (((int32_t)(sample) >> 13) - 240200)
 
 typedef struct {
-  uint32_t blocksize      : 12;
-  uint32_t datalen        : 12;
-  uint32_t unused         : 5;
-  uint32_t sub_sof        : 1;
-  uint32_t eof            : 1;
+  uint32_t blocksize : 12;
+  uint32_t datalen : 12;
+  uint32_t unused : 5;
+  uint32_t sub_sof : 1;
+  uint32_t eof : 1;
   volatile uint32_t owner : 1;
 
   uint32_t *buf_ptr;
   uint32_t *next_link_ptr;
 } sdio_queue_t;
+
+// Data we push to 'samples_queue'
+struct sum_queue_t {
+  // Sum of squares of mic samples, after Equalizer filter
+  float sum_sqr_SPL;
+  // Sum of squares of weighted mic samples
+  float sum_sqr_weighted;
+  // Debug only, FreeRTOS ticks we spent processing the I2S data
+  uint32_t proc_ticks;
+};
 
 static sdio_queue_t i2s_slc_items[SLC_BUF_CNT];  // I2S DMA buffer descriptors
 static uint32_t *i2s_slc_buf_pntr[SLC_BUF_CNT];  // Pointer to the I2S DMA buffer data
@@ -58,15 +70,173 @@ static volatile uint32_t rx_buf_cnt = 0;
 static volatile uint32_t rx_buf_idx = 0;
 static volatile bool rx_buf_flag = false;
 
+#define LEQ_PERIOD 1           // second(s)
+#define WEIGHTING A_weighting  // Also avaliable: 'C_weighting' or 'None' (Z_weighting)
+#define LEQ_UNITS "LAeq"       // customize based on above weighting used
+#define DB_UNITS "dBA"         // customize based on above weighting used
+#define USE_DISPLAY 0          // Use display (1) or don't (0)
+
+// NOTE: Some microphones require at least DC-Blocker filter
+#define MIC_EQUALIZER SPH0645LM4H_B_RB  // See below for defined IIR filters or set to 'None' to disable
+#define MIC_OFFSET_DB 3.0103            // Default offset (sine-wave RMS vs. dBFS). Modify this value for linear calibration
+
+// Customize these values from microphone datasheet
+#define MIC_SENSITIVITY -26    // dBFS value expected at MIC_REF_DB (Sensitivity value from datasheet)
+#define MIC_REF_DB 94.0        // Value at which point sensitivity is specified in datasheet (dB)
+#define MIC_OVERLOAD_DB 120.0  // dB - Acoustic overload point
+#define MIC_NOISE_DB 29        // dB - Noise floor
+#define MIC_BITS 24            // valid number of bits in I2S data
+#define MIC_CONVERT(s) (s >> (SAMPLE_BITS - MIC_BITS))
+#define MIC_TIMING_SHIFT 0  // Set to one to fix MSB timing for some microphones, i.e. SPH0645LM4H-x
+
+//
+// Sampling
+//
+#define SAMPLE_RATE 48000  // Hz, fixed to design of IIR filters
+#define SAMPLE_BITS 32     // bits
+#define SAMPLE_T int32_t
+#define SAMPLES_SHORT (SAMPLE_RATE / 8)  // ~125ms
+#define SAMPLES_LEQ (SAMPLE_RATE * LEQ_PERIOD)
+#define DMA_BANK_SIZE (SAMPLES_SHORT / 16)
+#define DMA_BANKS 32
+
+// Calculate reference amplitude value at compile time
+constexpr double MIC_REF_AMPL = pow(10, double(MIC_SENSITIVITY) / 20) * ((1 << (MIC_BITS - 1)) - 1);
+
+//
+// IIR Filters
+//
+
+// DC-Blocker filter - removes DC component from I2S data
+// See: https://www.dsprelated.com/freebooks/filters/DC_Blocker.html
+// a1 = -0.9992 should heavily attenuate frequencies below 10Hz
+SOS_IIR_Filter DC_BLOCKER = {
+  gain: 1.0,
+  sos: { { -1.0, 0.0, +0.9992, 0 } }
+};
+
+//
+// Equalizer IIR filters to flatten microphone frequency response
+// See respective .m file for filter design. Fs = 48Khz.
+//
+// Filters are represented as Second-Order Sections cascade with assumption
+// that b0 and a0 are equal to 1.0 and 'gain' is applied at the last step
+// B and A coefficients were transformed with GNU Octave:
+// [sos, gain] = tf2sos(B, A)
+// See: https://www.dsprelated.com/freebooks/filters/Series_Second_Order_Sections.html
+// NOTE: SOS matrix 'a1' and 'a2' coefficients are negatives of tf2sos output
+//
+
+// TDK/InvenSense ICS-43434
+// Datasheet: https://www.invensense.com/wp-content/uploads/2016/02/DS-000069-ICS-43434-v1.1.pdf
+// B = [0.477326418836803, -0.486486982406126, -0.336455844522277, 0.234624646917202, 0.111023257388606];
+// A = [1.0, -1.93073383849136326, 0.86519456089576796, 0.06442838283825100, 0.00111249298800616];
+SOS_IIR_Filter ICS43434 = {
+  gain: 0.477326418836803,
+  sos: { // Second-Order Sections {b1, b2, -a1, -a2}
+         { +0.96986791463971267, 0.23515976355743193, -0.06681948004769928, -0.00111521990688128 },
+         { -1.98905931743624453, 0.98908924206960169, +1.99755331853906037, -0.99755481510122113 } }
+};
+
+// TDK/InvenSense ICS-43432
+// Datasheet: https://www.invensense.com/wp-content/uploads/2015/02/ICS-43432-data-sheet-v1.3.pdf
+// B = [-0.45733702338341309   1.12228667105574775  -0.77818278904413563, 0.00968926337978037, 0.10345668405223755]
+// A = [1.0, -3.3420781082912949, 4.4033694320978771, -3.0167072679918010, 1.2265536567647031, -0.2962229189311990, 0.0251085747458112]
+SOS_IIR_Filter ICS43432 = {
+  gain: -0.457337023383413,
+  sos: { // Second-Order Sections {b1, b2, -a1, -a2}
+         { -0.544047931916859, -0.248361759321800, +0.403298891662298, -0.207346186351843 },
+         { -1.909911869441421, +0.910830292683527, +1.790285722826743, -0.804085812369134 },
+         { +0.000000000000000, +0.000000000000000, +1.148493493802252, -0.150599527756651 } }
+};
+
+// TDK/InvenSense INMP441
+// Datasheet: https://www.invensense.com/wp-content/uploads/2015/02/INMP441.pdf
+// B ~= [1.00198, -1.99085, 0.98892]
+// A ~= [1.0, -1.99518, 0.99518]
+SOS_IIR_Filter INMP441 = {
+  gain: 1.00197834654696,
+  sos: { // Second-Order Sections {b1, b2, -a1, -a2}
+         { -1.986920458344451, +0.986963226946616, +1.995178510504166, -0.995184322194091 } }
+};
+
+// Infineon IM69D130 Shield2Go
+// Datasheet: https://www.infineon.com/dgdl/Infineon-IM69D130-DS-v01_00-EN.pdf?fileId=5546d462602a9dc801607a0e46511a2e
+// B ~= [1.001240684967527, -1.996936108836337, 0.995703101823006]
+// A ~= [1.0, -1.997675693595542, 0.997677044195563]
+// With additional DC blocking component
+SOS_IIR_Filter IM69D130 = {
+  gain: 1.00124068496753,
+  sos: {
+    { -1.0, 0.0, +0.9992, 0 },  // DC blocker, a1 = -0.9992
+    { -1.994461610298131, 0.994469278738208, +1.997675693595542, -0.997677044195563 } }
+};
+
+// Knowles SPH0645LM4H-B, rev. B
+// https://cdn-shop.adafruit.com/product-files/3421/i2S+Datasheet.PDF
+// B ~= [1.001234, -1.991352, 0.990149]
+// A ~= [1.0, -1.993853, 0.993863]
+// With additional DC blocking component
+SOS_IIR_Filter SPH0645LM4H_B_RB = {
+  gain: 1.00123377961525,
+  sos: {                             // Second-Order Sections {b1, b2, -a1, -a2}
+         { -1.0, 0.0, +0.9992, 0 },  // DC blocker, a1 = -0.9992
+         { -1.988897663539382, +0.988928479008099, +1.993853376183491, -0.993862821429572 } }
+};
+
+//
+// Weighting filters
+//
+
+//
+// A-weighting IIR Filter, Fs = 48KHz
+// (By Dr. Matt L., Source: https://dsp.stackexchange.com/a/36122)
+// B = [0.169994948147430, 0.280415310498794, -1.120574766348363, 0.131562559965936, 0.974153561246036, -0.282740857326553, -0.152810756202003]
+// A = [1.0, -2.12979364760736134, 0.42996125885751674, 1.62132698199721426, -0.96669962900852902, 0.00121015844426781, 0.04400300696788968]
+SOS_IIR_Filter A_weighting = {
+  gain: 0.169994948147430,
+  sos: { // Second-Order Sections {b1, b2, -a1, -a2}
+         { -2.00026996133106, +1.00027056142719, -1.060868438509278, -0.163987445885926 },
+         { +4.35912384203144, +3.09120265783884, +1.208419926363593, -0.273166998428332 },
+         { -0.70930303489759, -0.29071868393580, +1.982242159753048, -0.982298594928989 } }
+};
+
+//
+// C-weighting IIR Filter, Fs = 48KHz
+// Designed by invfreqz curve-fitting, see respective .m file
+// B = [-0.49164716933714026, 0.14844753846498662, 0.74117815661529129, -0.03281878334039314, -0.29709276192593875, -0.06442545322197900, -0.00364152725482682]
+// A = [1.0, -1.0325358998928318, -0.9524000181023488, 0.8936404694728326   0.2256286147169398  -0.1499917107550188, 0.0156718181681081]
+SOS_IIR_Filter C_weighting = {
+  gain: -0.491647169337140,
+  sos: {
+    { +1.4604385758204708, +0.5275070373815286, +1.9946144559930252, -0.9946217070140883 },
+    { +0.2376222404939509, +0.0140411206016894, -1.3396585608422749, -0.4421457807694559 },
+    { -2.0000000000000000, +1.0000000000000000, +0.3775800047420818, -0.0356365756680430 } }
+};
+
+#if (MIC_TIMING_SHIFT > 0)
+// Undocumented (?!) manipulation of I2S peripheral registers
+// to fix MSB timing issues with some I2S microphones
+REG_SET_BIT(I2S_TIMING_REG(I2S_PORT), BIT(9));
+REG_SET_BIT(I2S_CONF_REG(I2S_PORT), I2S_RX_MSB_SHIFT);
+#endif
+
 void i2s_init();
 void slc_init();
 void i2s_set_rate(uint32_t rate);
 void slc_isr(void *para);
 
+// Static buffer for block of samples
+// float samples[SAMPLES_SHORT] __attribute__((aligned(4)));
+
+sum_queue_t q;
+uint32_t Leq_samples = 0;
+double Leq_sum_sqr = 0;
+double Leq_dB = 0;
+
+
 /* Main -----------------------------------------------------------------------*/
-void
-setup()
-{
+void setup() {
   Serial.println("START");
   rx_buf_cnt = 0;
 
@@ -83,27 +253,61 @@ setup()
   i2s_init();
 }
 
-void
-loop()
-{
+void loop() {
   int32_t value;
   char withScale[256];
+  float samples[SLC_BUF_LEN];
 
   if (rx_buf_flag) {
+
+    // Convert (including shifting) integer microphone values to floats,
+    // using the same buffer (assumed sample size is same as size of float),
+    // to save a bit of memory
+    // SAMPLE_T *int_samples = (SAMPLE_T *)&samples;
+    // for (int i = 0; i < SAMPLES_SHORT; i++) samples[i] = MIC_CONVERT(int_samples[i]);
+
     for (int x = 0; x < SLC_BUF_LEN; x++) {
       if (i2s_slc_buf_pntr[rx_buf_idx][x] > 0) {
-#ifdef DEBUG
-        Serial.print(i2s_slc_buf_pntr[rx_buf_idx][x], BIN);
-        Serial.println("");
-#else
-        value = convert(i2s_slc_buf_pntr[rx_buf_idx][x]);
-        // sprintf(withScale, "-1 %f 1", (float)value / 4096.0f);
-        // Serial.println(withScale);
-        Serial.println(value * -1);
-#endif
+        samples[x] = MIC_CONVERT(i2s_slc_buf_pntr[rx_buf_idx][x]);
+        // value = i2s_slc_buf_pntr[rx_buf_idx][x];
       }
     }
-    rx_buf_flag = false;
+    sum_queue_t q;
+    // Apply equalization and calculate Z-weighted sum of squares,
+    // writes filtered samples back to the same buffer.
+    q.sum_sqr_SPL = MIC_EQUALIZER.filter(samples, samples, SLC_BUF_LEN);
+
+    // Apply weighting and calucate weigthed sum of squares
+    q.sum_sqr_weighted = WEIGHTING.filter(samples, samples, SLC_BUF_LEN);
+
+    double rmssqr = double(q.sum_sqr_SPL) / SLC_BUF_LEN;
+    double short_RMS = sqrt(rmssqr);
+    double short_SPL_dB = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(short_RMS / MIC_REF_AMPL); // TODO - Resolves to NaN ????
+
+    // In case of acoustic overload or below noise floor measurement, report infinty Leq value
+    if (short_SPL_dB > MIC_OVERLOAD_DB) {
+      Leq_sum_sqr = INFINITY;
+    } else if (isnan(short_SPL_dB) || (short_SPL_dB < MIC_NOISE_DB)) {
+      Leq_sum_sqr = -INFINITY;
+    }
+
+    // Accumulate Leq sum
+    Leq_sum_sqr += q.sum_sqr_weighted;
+    Leq_samples += SAMPLES_SHORT;
+
+
+    // When we gather enough samples, calculate new Leq value
+    if (Leq_samples >= SAMPLE_RATE * LEQ_PERIOD) {
+      double Leq_RMS = sqrt(Leq_sum_sqr / Leq_samples);
+      Leq_dB = MIC_OFFSET_DB + MIC_REF_DB + 20 * log10(Leq_RMS / MIC_REF_AMPL);
+      Leq_sum_sqr = 0;
+      Leq_samples = 0;
+
+      // Serial output, customize (or remove) as needed
+      Serial.printf("%.1f\n", Leq_dB);
+
+      rx_buf_flag = false;
+    }
   }
 }
 
@@ -112,9 +316,7 @@ loop()
 /**
  * Initialise I2S as a RX master.
  */
-void
-i2s_init()
-{
+void i2s_init() {
   // Config RX pin function
   PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTDI_U, FUNC_I2SI_DATA);
   PIN_FUNC_SELECT(PERIPHS_IO_MUX_MTCK_U, FUNC_I2SI_BCK);
@@ -154,15 +356,13 @@ i2s_init()
  * I2S bits mode only has space for 15 extra bits,
  * 31 in total. The
  */
-void
-i2s_set_rate(uint32_t rate)
-{
+void i2s_set_rate(uint32_t rate) {
   uint32_t i2s_clock_div = (I2S_CLK_FREQ / (rate * 31 * 2)) & I2SCDM;
   uint32_t i2s_bck_div = (I2S_CLK_FREQ / (rate * i2s_clock_div * 31 * 2)) & I2SBDM;
 
 #ifdef DEBUG
   Serial.printf("Rate %u Div %u Bck %u Freq %u\n",
-  rate, i2s_clock_div, i2s_bck_div, I2S_CLK_FREQ / (i2s_clock_div * i2s_bck_div * 31 * 2));
+                rate, i2s_clock_div, i2s_bck_div, I2S_CLK_FREQ / (i2s_clock_div * i2s_bck_div * 31 * 2));
 #endif
 
   // RX master mode, RX MSB shift, right first, msb right
@@ -175,9 +375,7 @@ i2s_set_rate(uint32_t rate)
  * Counter intuitively, we use the TXLINK here to
  * receive data.
  */
-void
-slc_init()
-{
+void slc_init() {
   for (int x = 0; x < SLC_BUF_CNT; x++) {
     i2s_slc_buf_pntr[x] = (uint32_t *)malloc(SLC_BUF_LEN * 4);
     for (int y = 0; y < SLC_BUF_LEN; y++) i2s_slc_buf_pntr[x][y] = 0;
@@ -199,9 +397,9 @@ slc_init()
   SLCIC = 0xFFFFFFFF;
 
   // Configure DMA
-  SLCC0 &= ~(SLCMM << SLCM);      // Clear DMA MODE
-  SLCC0 |= (1 << SLCM);           // Set DMA MODE to 1
-  SLCRXDC |= SLCBINR | SLCBTNR;   // Enable INFOR_NO_REPLACE and TOKEN_NO_REPLACE
+  SLCC0 &= ~(SLCMM << SLCM);     // Clear DMA MODE
+  SLCC0 |= (1 << SLCM);          // Set DMA MODE to 1
+  SLCRXDC |= SLCBINR | SLCBTNR;  // Enable INFOR_NO_REPLACE and TOKEN_NO_REPLACE
 
   // Feed DMA the 1st buffer desc addr
   SLCTXL &= ~(SLCTXLAM << SLCTXLA);
@@ -222,8 +420,7 @@ slc_init()
  * to one of the buffers.
  */
 void ICACHE_RAM_ATTR
-slc_isr(void *para)
-{
+slc_isr(void *para) {
   uint32_t status;
 
   status = SLCIS;
@@ -236,7 +433,7 @@ slc_isr(void *para)
   if (status & SLCITXEOF) {
     // We have received a frame
     ETS_SLC_INTR_DISABLE();
-    sdio_queue_t *finished = (sdio_queue_t*)SLCTXEDA;
+    sdio_queue_t *finished = (sdio_queue_t *)SLCTXEDA;
 
     finished->eof = 0;
     finished->owner = 1;
@@ -252,4 +449,3 @@ slc_isr(void *para)
     ETS_SLC_INTR_ENABLE();
   }
 }
-
